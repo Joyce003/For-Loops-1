28235e4bbabefb7e9a78e2834337bb72b8a3f406
Joyce003 <jpirtle2348@gmail.com>
2024-02-07 22:45:33 -0600

    added array

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ for(let i = 0; i <arr.length; i++) {
 }
 //let i = 0 is our initializaiton. i < 5 is our condition.
 //if is less than the length of the arrary (arr.length), we are going to continue going through the array. So once we get to the end of the array, we'll be done witht the for loop. At the end we're going to incerement i by 1. 
+
+// nested for loops 
+let arr = [
+    [1,2], [3,4], [5,6]
+];
