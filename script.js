d3ddd39998a89d36d2950b17ea6492beacabcde8
Joyce003 <jpirtle2348@gmail.com>
2024-02-07 22:38:18 -0600

    added array

--- script.js
+++ script.js
@@ -3,5 +3,13 @@
 
 let ourArray = [];
 for (let i = 0; i < 5; i++) {
+    if (i > 2) break;
     ourArrray.push(i); 
 }
+console.log(ourArray);
+
+let arr = [10,9,8,7,6]; 
+for(let i = 0; i <arr.length; i++) {
+    
+}
+//let i = 0 is our initializaiton. i < 5 is our condition.
